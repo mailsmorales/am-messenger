4c235879f8e0660e18d030d50b7a38cb08c18308
mukhamed-Ali <mailsmorales21@mail.ru>
2022-05-07 14:20:59 +0600

    auth fixed

--- src/pages/register/Auth.jsx
+++ src/pages/register/Auth.jsx
@@ -1,36 +1,72 @@
-import React from 'react'
-import TextField from '@mui/material/TextField';
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
 const Register = () => {
-   
-    
-    
-    return (
-        <>
-        <h4>Регистрация</h4>
-        <TextField
-         id="standard-basic"
-         label="Your name"
-         required
-         variant="standard"
-         />
-        <TextField
-         id="standard-basic"
-         label="Email"
-         required
-         variant="standard"
-         />
-        <TextField
-        id="standard-password-input"
-        label="Password"
-        type="password"
-        autoComplete="current-password"
-        variant="standard"
-        />
-        <Button sx={{marginTop:1, width:195, marginBottom:1}}  className="accept" variant="contained">Подтвердить</Button>
-        </>
-    )
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: null,
+    loading: false,
+  });
+  const { name, email, password, error, loading } = data;
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+
+const handleSubmit = async e => {
+e.preventDefault();
+if (!name || !email || password) {
+  setData({...data, error: "Все поля обязательны"})
+}
 }
 
-export default Register
+  return (
+    <>
+      <h4>Регистрация</h4>
+      <form className="form" onSubmit={handleSubmit}>
+        <TextField
+          label="Your name"
+          name="name"
+          value={name}
+          onChange={handleChange}
+          required
+          variant="standard"
+        />
+        <TextField
+          id="standard-basic"
+          label="Email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+          required
+          variant="standard"
+        />
+        <TextField
+          id="standard-password-input"
+          label="Password"
+          type="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+          autoComplete="current-password"
+          variant="standard"
+        />
+        {error ? <p className="error"> {error}</p>: null}
+        <Button
+          sx={{ marginTop: 1, width: 195, marginBottom: 1 }}
+          className="accept"
+          variant="contained"
+          type="submit"
+        >
+          Подтвердить
+        </Button>
+      </form>
+    </>
+  );
+};
+
+export default Register;
