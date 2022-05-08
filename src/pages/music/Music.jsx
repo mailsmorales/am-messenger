efa7c5427f74f12a734b384f890420276b3ac15b
korupsk <korups1996@gmail.com>
2022-05-09 00:17:29 +0600

    add-new-page-music

---
+++ src/pages/music/Music.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "../../components/sidebar/Sidebar";
+
+export default function Music() {
+  return (
+    <>
+    <Sidebar/>
+      <div className="musicContainer">
+        adwdawfgbnhmj
+      </div>
+    </>
+  );
+}
