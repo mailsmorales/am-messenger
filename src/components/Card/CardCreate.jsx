00d05920ddab24f7bc430190355f613dc6393c68
Maratoos <kudushev01@mail.ru>
2022-05-11 00:15:00 +0600

    new-prepare-to-crud-operations

---
+++ src/components/Card/CardCreate.jsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
+import Button from "@mui/material/Button";
+import MoodIcon from "@mui/icons-material/Mood";
+import Avatar from "@mui/material/Avatar";
+import Img from "../../assets/img/ali.jpg";
+import { storage, db, auth } from "../../firebase/config";
+import { ref, getDownloadURL, uploadBytes } from "firebase/storage";
+import { getDoc, doc, updateDoc } from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
+
+const CardCreate = () => {
+  const [img, setImg] = useState("");
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (_user) => {
+      if (!user) getDoc(doc(db, "users", _user.uid)).then((docSnap) => setUser(docSnap.data())
+    )
+    });
+
+    return () => {
+      unsub();
+    };
+  }, []);
+  useEffect(() => {
+    if (img) {
+      const uploadImg = async () => {
+        const imgRef = ref(
+          storage,
+          `avatar/${new Date().getTime()} - ${img.name}`
+        );
+        try {
+          const snap = await uploadBytes(imgRef, img);
+          const url = await getDownloadURL(ref(storage, snap.ref.fullPath));
+
+          await updateDoc(doc(db, "users", auth.currentUser.uid), {
+            avatar: url,
+            avatarPath: snap.ref.fullPath,
+          });
+          console.log(url);
+          setImg("");
+        } catch (err) {
+          console.log(err.message);
+        }
+      };
+      uploadImg();
+    }
+  }, [img]);
+
+  return (
+    <Box className="postBox" sx={{ width: 915, height: 240, backgroundColor: "primary.white", borderRadius: "5px",}}>
+        {user && (
+            <>
+            <div className="postBoxTop">
+              <Avatar
+                alt="Remy Sharp"
+                src={user.avatar || Img}
+                sx={{ width: 50, height: 50 }}
+              />
+              <p>О чем вы думаете {user.name} ?</p>
+            </div>
+            <div className="postBoxMid">
+              <TextField
+                sx={{
+                  marginLeft: "50px",
+                  width: "90%",
+                }}
+                required
+                id="standard-basic"
+                variant="standard"
+              />
+            </div>
+            <div className="postBoxBot">
+              <label>
+                <AddPhotoAlternateIcon className="addPhotoIcon" />
+                <input className="profileInputFile" type="file" />
+                <p>Фото</p>
+              </label>
+              <div>
+                <MoodIcon
+                  sx={{ marginLeft: "10px" }}
+                  className="addSmileIcon"
+                />
+                <p>Смайлики</p>
+              </div>
+              <Button
+                sx={{ marginLeft: "64%", height: "40px" }}
+                variant="contained"
+                color="primary"
+              >
+                Опубликовать
+              </Button>
+            </div>
+            </>
+        )}
+    </Box>
+  )
+}
+
+export default CardCreate
