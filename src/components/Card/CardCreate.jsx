5908561288da70c39aaade2b491f080537727c8c
Maratoos <kudushev01@mail.ru>
2022-05-11 15:24:57 +0600

    crud-operation-dont-finished

--- src/components/Card/CardCreate.jsx
+++ src/components/Card/CardCreate.jsx
@@ -6,14 +6,16 @@ import Button from "@mui/material/Button";
 import MoodIcon from "@mui/icons-material/Mood";
 import Avatar from "@mui/material/Avatar";
 import Img from "../../assets/img/ali.jpg";
+import CircularStatic from '../Progress/CircularStatic'
 import { storage, db, auth } from "../../firebase/config";
-import { ref, getDownloadURL, uploadBytes } from "firebase/storage";
-import { getDoc, doc, updateDoc } from "firebase/firestore";
+import { ref, getDownloadURL, uploadBytes, uploadBytesResumable } from "firebase/storage";
+import { getDoc, doc, updateDoc, Timestamp, collection } from "firebase/firestore";
 import { onAuthStateChanged } from "firebase/auth";
 
 const CardCreate = () => {
   const [img, setImg] = useState("");
   const [user, setUser] = useState();
+  const [progress, setProgress] = useState(0)
 
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (_user) => {
@@ -50,6 +52,53 @@ const CardCreate = () => {
     }
   }, [img]);
 
+  const [formData, setFormData] = useState({
+    description:"",
+    image:"",
+    createdAt: Timestamp.now().toDate()
+  })
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value})
+  }
+
+  const handleImageChange = (e) => {
+    setFormData({...formData, image: e.target.file[0] })
+  }
+
+  const handlePublish = () => {
+    if(!formData.description || formData.image) {
+      console.log("you need to fill all fields")
+    }
+
+    const storageRef = ref(storage, `/postsImages/${Date.now()}${formData.image.name}`);
+
+    const uploadImage = uploadBytesResumable(storageRef, formData.image)
+
+    uploadImage.on("state_changed",
+    (snapshot) => {
+      const progressPercent = Math.round(
+        (snapshot.bytesTrasferred /snapshot.totalBytes) * 100
+      );
+      setProgress(progressPercent)
+    },
+    (err)=>{
+      console.log(err)
+    },
+    ()=>{
+      setFormData({
+        description:"",
+        image:""
+      });
+
+      getDownloadURL(uploadImage.snapshot.ref)
+      .then((url) => {
+        const postRef = collection()
+      })
+    }
+    )
+  }
+
   return (
     <Box className="postBox" sx={{ width: '100%', height: '30%', backgroundColor: "primary.white", borderRadius: "5px",}}>
         {user && (
@@ -70,15 +119,23 @@ const CardCreate = () => {
                   width: "90%",
                 }}
                 required
+                name="description"
                 id="standard-basic"
                 variant="standard"
+                value={formData.description}
+                onChange={(e) =>handleChange(e)}
               />
             </div>
             <div className="postBoxBot">
                 <div className="postBoxBotRow">
                 <label>
                 <AddPhotoAlternateIcon className="addPhotoIcon" />
-                <input className="profileInputFile" type="file" />
+                <input
+                className="profileInputFile"
+                type="file" 
+                name="image" 
+                accept="image/*" 
+                onChange={(e) =>handleImageChange(e)} />
                 <p>Фото</p>
               </label>
               <div>
@@ -94,10 +151,12 @@ const CardCreate = () => {
                 variant="contained"
                 color="primary"
                 className="postBoxBotBtn"
+                onClick={handlePublish}
               >
                 Опубликовать
               </Button>
             </div>
+            <CircularStatic />
             </>
         )}
     </Box>
